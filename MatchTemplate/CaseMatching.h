ebb7e6d7dc552facdab977074c99508139a09094
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2022-12-14 23:39:12 +0100

    1

--- MatchTemplate/CaseMatching.h
+++ MatchTemplate/CaseMatching.h
@@ -12,11 +12,13 @@ using namespace cv;
 class CaseMatching
 {
 	void OpenCaseAndTakeScreen(vector<POINT> tempPoints, Mat templ, std::string NameOfCase, int page);
+	void MatchingCaseInCase(vector<Mat>& MatScreenVector, vector<POINT> tempPoints, int page);
 	void MoveTopBarTHICCcase();
 
 	vector<vector<POINT>> PointVectorTemp;
 	vector<vector<POINT>> PointVectorCleanUp;
-	void THICC() {
+
+	void THICCcase() {
 		Mat templ;
 		array<Mat, 11> ReturntMatScreen;
 		vector<POINT> ReturnPoints;
@@ -25,7 +27,7 @@ class CaseMatching
 		String NameOfCase = "THICCcase";
 
 		for (int i = 0; i < 7; i++) {// 5 must later be size 
-			TemplateMatching::templateMatchingItems("ObjectImages/THICCcase.png", 0.90, false, false, "THICCcase", ReturnPoints, ReturntMatScreen[i]);
+			TemplateMatching::templateMatchingItems("ObjectImages/THICCcase.png", 0.90, false, false, NameOfCase, ReturnPoints, ReturntMatScreen[i]);
 			if (!ReturnPoints.empty()) {
 				PointVectorTemp.push_back(ReturnPoints);
 				ReturnPoints.clear();
@@ -51,9 +53,89 @@ class CaseMatching
 			}
 			page++;
 		}
+		PointVectorTemp.clear();
+		PointVectorCleanUp.clear();
+	}
+
+	void ItemsCase() {
+		Mat templ;
+		array<Mat, 11> ReturntMatScreen;
+		vector<POINT> ReturnPoints;
+		ReturntMatScreen = StartUp::TakeScreenshots();
+		templ = imread("ObjectImages/ItemsCase.png");
+		String NameOfCase = "ItemsCase";
+
+		for (int i = 0; i < 7; i++) {// 5 must later be size 
+			TemplateMatching::templateMatchingItems("ObjectImages/ItemsCase.png", 0.90, false, false, NameOfCase, ReturnPoints, ReturntMatScreen[i]);
+			if (!ReturnPoints.empty()) {
+				PointVectorTemp.push_back(ReturnPoints);
+				ReturnPoints.clear();
+			}
+			else {
+				PointVectorTemp.push_back(ReturnPoints);
+				ReturnPoints.clear();
+			}
+			cout << "-----------------------------------------------------------------------------------\n";
+		}
+
+		StartUp::cleanUpVector(PointVectorTemp, PointVectorCleanUp);
+		StartUp::CheckScrollbarPositions();
+
+		int page = 0;
+		for (int i = 0; i < PointVectorCleanUp.size(); i++) {
+			if (!PointVectorCleanUp[i].size() == 0) {
+				OpenCaseAndTakeScreen(PointVectorCleanUp[i], templ, NameOfCase, page);
+			}
+			else {
+				float keyforInput = 0x28;// virtual-key code for the "DOWN ARROW" key
+				Keyboard::KeyboardInput(keyforInput);
+			}
+			page++;
+		}
+		PointVectorTemp.clear();
+		PointVectorCleanUp.clear();
 	}
 	
+	std::array<std::string, 8> Cases{
+		"itemImages/CaseImages/AmmoCase.png",
+		"itemImages/CaseImages/GrenadCase.png",
+		"itemImages/CaseImages/HolodilnickCase.png",
+		"itemImages/CaseImages/MagCase.png",
+		"itemImages/CaseImages/MedCase.png",
+		"itemImages/CaseImages/MoneyCase.png",
+		"itemImages/CaseImages/JunkCase.png",
+		"itemImages/CaseImages/WeaponsCase.png",
+	};
+
+	std::array<std::string, 8> NameOfItemCases{
+		"AmmoCase",
+		"GrenadCase",
+		"HolodilnickCase",
+		"MagCase",
+		"MedCase",
+		"MoneyCase",
+		"JunkCase",
+		"WeaponsCase",
+	};
+
+	std::array<double, 8> CasesThreshold{
+		0.90,//AmmoCase
+		0.90,//GrenadCase
+		0.90,//HolodilnickCase
+		0.90,//MagCase
+		0.90,//MedCase
+		0.90,//MoneyCase
+		0.90,//JunkCase
+		0.90,//WeaponsCase
+	};
+
+	void OtherCases() {
+
+	}
+
+
 	vector<vector<PointCaseInStash>> pointCaseInStash;
+	vector<vector<PointCaseInCase>> pointCaseInCase;
 
 	void OpenCaseAndTakeScreen(vector<POINT> tempPoints, Mat templ, std::string NameOfCase, int page) {
 		Color color;
@@ -70,28 +152,11 @@ class CaseMatching
 		for (int i = 0; i < tempPoints.size(); i++) {
 			Rect Rec(tempPoints[i].x, tempPoints[i].y, templ.cols, templ.rows);
 			color = TemplateMatching::ColorMatching(Rec, MatScreen);
-			switch (color)
-			{
-				case Color::RED:
-					pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, Color::RED, templ.rows, templ.cols, page);
-				case Color::ORANGSH:
-					pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, Color::ORANGSH, templ.rows, templ.cols, page);
-				case Color::GREEN:
-					pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, Color::GREEN, templ.rows, templ.cols, page);
-				case Color::BLUE:
-					pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, Color::BLUE, templ.rows, templ.cols, page);
-				case Color::PURPLE:
-					pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, Color::PURPLE, templ.rows, templ.cols, page);
-				case Color::PINK:
-					pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, Color::PINK, templ.rows, templ.cols, page);
-				case Color::GRAY:
-					pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, Color::GRAY, templ.rows, templ.cols, page);
-				case Color::NOCOLOR:
-					pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, Color::NOCOLOR, templ.rows, templ.cols, page);
-			}
+			pointCasetempStash.emplace_back(tempPoints[i], NameOfCase, color, templ.rows, templ.cols, page);
 		}
 		
 		pointCaseInStash.emplace_back(pointCasetempStash);
+		pointCasetempStash.clear();
 		
 		POINT point{};
 		for (int i = 0; i < pointCasetempStash.size(); i++) {
@@ -109,15 +174,14 @@ class CaseMatching
 
 			float keyforInput = 0x1B;// virtual-key code for the "ESC button" key
 			Keyboard::KeyboardInput(keyforInput);
-
-			
 		}
 
-		MatchingCase(MatScreenVector, tempPoints);
+		MatchingCaseInCase(MatScreenVector, tempPoints, page);
 	}
 
 
-	std::array<std::string, 6> Cases{
+
+	std::array<std::string, 6> CasesInCase{
 		"itemImages/CaseImages/AmmoCase.png",
 		"itemImages/CaseImages/GrenadCase.png",
 		"itemImages/CaseImages/HolodilnickCase.png",
@@ -126,7 +190,7 @@ class CaseMatching
 		"itemImages/CaseImages/MoneyCase.png",
 	};
 
-	std::array<std::string, 6> NameOfItemCases{
+	std::array<std::string, 6> NameOfItemCasesInCase{
 		"AmmoCase",
 		"GrenadCase",
 		"HolodilnickCase",
@@ -135,7 +199,7 @@ class CaseMatching
 		"MoneyCase",
 	};
 
-	std::array<double, 6> CasesThreshold{
+	std::array<double, 6> CasesInCaseThreshold{
 		0.90,//AmmoCase
 		0.90,//GrenadCase
 		0.90,//HolodilnickCase
@@ -144,26 +208,32 @@ class CaseMatching
 		0.90,//MoneyCase
 	};
 
-	
-
-	void MatchingCase(vector<Mat> MatScreenVector, vector<POINT> tempPoints) {
+	void MatchingCaseInCase(vector<Mat> &MatScreenVector, vector<POINT> tempPoints, int page) {
 		string  filename, templatename;
 		double	threshold;
-		int size = sizeof(Cases) / sizeof(string);
-		vector<PointCaseInCase> pointCaseInCase;
+		int size = sizeof(CasesInCase) / sizeof(string);
+		vector<PointCaseInCase> pointCaseInCasetemp;
+		Mat templ;
+		Color color{};
 
 		vector<POINT> ReturnDataCase;
 		for (int i = 0; i < MatScreenVector.size(); i++) {
 			for (int i2 = 0; i2 < size; i2++) {
-				threshold = CasesThreshold[i2];
-				templatename = Cases[i2];
+				threshold = CasesInCaseThreshold[i2];
+				templatename = CasesInCase[i2];
+				templ = imread(CasesInCase[i2]);
 
-				TemplateMatching::templateMatchingItems(templatename, threshold, false, true, NameOfItemCases[i], ReturnDataCase, MatScreenVector[i]);
+				TemplateMatching::templateMatchingItems(templatename, threshold, false, true, NameOfItemCasesInCase[i], ReturnDataCase, MatScreenVector[i]);
 
 				for (int i3 = 0; i3 < ReturnDataCase.size(); i3++) {
-					pointCaseInCase.emplace_back(ReturnDataCase[i3], tempPoints[i], NameOfItemCases[i], )
+					Rect Rec(ReturnDataCase[i3].x, ReturnDataCase[i3].y, templ.cols, templ.rows);
+
+					color = TemplateMatching::ColorMatching(Rec, MatScreenVector[i]);
+					pointCaseInCasetemp.emplace_back(ReturnDataCase[i3], tempPoints[i], NameOfItemCasesInCase[i], color, templ.rows, templ.cols, page);
 				}
 			}
+			pointCaseInCase.emplace_back(pointCaseInCasetemp);
+			pointCaseInCasetemp.clear();
 		}
 		
 	}

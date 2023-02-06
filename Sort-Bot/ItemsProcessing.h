e6f8d00dac25776136bc11fedaf1ab58a09cfd0c
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-02-06 21:56:23 +0100

    clean up vector barter updatet

--- Sort-Bot/ItemsProcessing.h
+++ Sort-Bot/ItemsProcessing.h
@@ -92,68 +92,44 @@ namespace ItemsProcessing {
 	}
 
 
-
-
-
-
-
-
-
-
 	void cleanUpVectorItemsBarter() {
 		vector<PointBarter> temp;
-		if (!pointBarter_NC.size() >= 1)
-			return;
 
 		temp = pointBarter_NC[0];
 		pointBarter_C.emplace_back(temp);
 		temp.clear();
 
-		pointBarter_NC.emplace_back(temp);
-
 		int iTemp = 1;
-		for (int i = 1; i < pointBarter_NC.size(); i++) {// Loop the vector
+		for (int i = 1; i < pointBarter_NC.size(); i++) {
 			iTemp++;
 
 			if (iTemp == pointBarter_NC.size())
 				break;
 
-			for (int i2 = 0; i2 < pointBarter_NC[i].size(); i2++) {//Loop through the vector of vector 
+			for (PointBarter pointBA : pointBarter_NC[i]) {
+				for (PointBarter inPointBA : pointBarter_NC[iTemp]) {
+					PointBarter tempPointBA = inPointBA;
+					tempPointBA.point.y = tempPointBA.point.y + 343;
 
-				for (int i3 = 0; i3 < pointBarter_NC[iTemp].size(); i3++) {//Loop through the next vector of vector  
-					bool FoundDuplicate = false;
+					int x_minus_1 = inPointBA.point.x - 1;
+					int x_plus_1 = inPointBA.point.x + 1;
 
-					POINT pointLookForLast = pointBarter_NC[i][i2].point;
-					pointLookForLast.y = 340 + pointLookForLast.y;
-					POINT pointTempGround = pointBarter_NC[iTemp][i3].point;
-					pointTempGround.y = 340 + pointTempGround.y;
-
-					if (pointLookForLast.y == pointBarter_NC[iTemp][i3].point.y && pointLookForLast.x == pointBarter_NC[iTemp][i3].point.x)
-						break;
-
-					for (int i4 = 0; i4 < 6; i4++) {//To run pointTemp from 340 to 345 and compare with the current point in the next vector of vector 
-						POINT pointTemp = pointTempGround;
-						pointTemp.y = pointTemp.y + i4;
-
-						if (pointBarter_NC[i][i2].point.y == pointTemp.y && pointBarter_NC[i][i2].point.x == pointTemp.x) {//Check if pointTemp is equal to the current point in the next vector of vector
-							FoundDuplicate = true;
-							break;
+					if (inPointBA.point.y == pointBA.point.y) {
+						if (inPointBA.point.x == pointBA.point.x || x_minus_1 == pointBA.point.x || x_plus_1 == pointBA.point.x) {
+							temp.emplace_back(pointBA);
 						}
-					}
-
-					if (FoundDuplicate == true) {//If a duplicate was found the point that was compared is pushed to temp
-						temp.emplace_back(pointBarter_NC[i][i2]);
 					}
 				}
 			}
-			if (!temp.empty()) {
-				pointBarter_C.emplace_back(temp);
-				temp.clear();
+			if (iTemp == 10) {
+				for (PointBarter pointBA : pointBarter_NC[iTemp]) {
+					if (pointBA.point.y >= 618) {
+						temp.emplace_back(pointBA);
+					}
+				}
 			}
-			else {
-				pointBarter_C.emplace_back(temp);
-				temp.clear();
-			}
+			pointBarter_C.emplace_back(temp);
+			temp.clear();
 		}
 	}
 }

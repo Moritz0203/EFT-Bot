a090dbe42eadb42bdd66ffe5d3bd87e94a2422b6
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-02-25 17:51:44 +0100

    ItemArraysHEXtoText

--- Sort-Bot/ItemArraysHEXtoText.h
+++ Sort-Bot/ItemArraysHEXtoText.h
@@ -1,1 +1,51 @@
 #pragma once
+#include <windows.h>
+#include <iostream>
+
+namespace ArraysText {
+	std::array<std::string, 7> Rus762x25{
+		"TTAKBS",
+		"TTFMJ43",
+		"TTLRN",
+		"TTLRNPC",
+		"TTP",
+		"TTPst",
+		"TTPT"
+	};
+
+	std::array<std::string, 5> Nato762x35{
+		"BlackoutAP",
+		"BlackoutBCPFMJ",
+		"BlackoutM62Tracer",
+		"BlackoutV-Max",
+		"Whisper"
+	};
+
+	std::array<std::string, 6> Rus762x39{
+		"BP",
+		"HP",
+		"MAIAP",
+		"PS",
+		"T45M1",
+		"US"
+	};
+
+	std::array<std::string, 7> Nato762x51{
+		"M80",
+		"M62",
+		"M61",
+		"M993",
+		"BCPFMJ",
+		"TCWSP",
+		"UltraNosi"
+	};
+
+	std::array<std::string, 6> Rus762x54R{
+		"RBS",
+		"RBT",
+		"RLPS",
+		"RPS",
+		"RSNB",
+		"RT-46M"
+	};
+}

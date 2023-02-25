3534cef3a70788ed0ef4f8c920e21700e50f536b
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-02-25 17:51:19 +0100

    Addet Item array Hex to Text header

--- Sort-Bot/InitializeMovPrefix.h
+++ Sort-Bot/InitializeMovPrefix.h
@@ -3,65 +3,31 @@
 #include <vector>
 #include <windows.h>
 #include "MovPrefixGlobalVector.h"
+#include "ItemArraysHEXtoText.h"
 using namespace std;
 
 namespace InitializeMovPrefix {
-	
+	static std::vector<MovPrefixGroup> movPrefixGroup;
+	static std::vector<MovPrefixItem> movPrefixItem;
+
 	void Initialize() {
 		string settingsForMoving;
 
 		cout << "Do you want to keep the default settings for moving the items or set your own \n( type 'yes' for I want to keep them, type 'no' to change them or type 'view' to see the default settings ). \n" << endl;
 		cout << ": ";
 		cin >> settingsForMoving;
+		setup();
 	}
 
 	void setup() {
-		
+		vector<Prefix> prefix;
+
+		prefix[0].tagOfCase = "Amo";
+
+		movPrefixGroup.emplace_back(ArraysText::Rus762x39, prefix);
+		movPrefixGroup.emplace_back(ArraysText::Nato762x51, prefix);
 	}
 
-	std::array<std::string, 7> Rus762x25{
-		"TTAKBS",
-		"TTFMJ43",
-		"TTLRN",
-		"TTLRNPC",
-		"TTP",
-		"TTPst",
-		"TTPT"
-	};
 
-	std::array<std::string, 5> Nato762x35{
-		"BlackoutAP",
-		"BlackoutBCPFMJ",
-		"BlackoutM62Tracer",
-		"BlackoutV-Max",
-		"Whisper"
-	};
-
-	std::array<std::string, 6> Rus762x39{
-		"BP",
-		"HP",
-		"MAIAP",
-		"PS",
-		"T45M1",
-		"US"
-	};
-
-	std::array<std::string, 7> Nato762x51{
-		"M80",
-		"M62",
-		"M61",
-		"M993",
-		"BCPFMJ",
-		"TCWSP",
-		"UltraNosi"
-	};
-
-	std::array<std::string, 6> Rus762x54R{
-		"RBS",
-		"RBT",
-		"RLPS",
-		"RPS",
-		"RSNB",
-		"RT-46M"
-	};
+	
 }
--- Sort-Bot/MovPrefixGlobalVector.h
+++ Sort-Bot/MovPrefixGlobalVector.h
@@ -6,6 +6,3 @@
 
 static std::vector<std::vector<MovPrefixGroup>> groupedMovPrefixGroup;
 static std::vector<std::vector<MovPrefixItem>> groupedMovPrefixItems;
-
-static std::vector<MovPrefixGroup> movPrefixGroup;
-static std::vector<MovPrefixItem> movPrefixItem;

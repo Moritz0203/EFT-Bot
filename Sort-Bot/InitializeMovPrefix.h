75919c8894cb7911fe9cd95d21811c4060ac0a2b
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-03-03 21:51:31 +0100

    addet comes

--- Sort-Bot/InitializeMovPrefix.h
+++ Sort-Bot/InitializeMovPrefix.h
@@ -15,9 +15,9 @@ namespace InitializeMovPrefix {
 	void Initialize() {
 		string settingsForMoving;
 
-		cout << "Do you want to keep the default settings for moving the items or set your own \n( type 'yes' for I want to keep them, type 'no' to change them or type 'view' to see the default settings ). \n" << endl;
+		/*cout << "Do you want to keep the default settings for moving the items or set your own \n( type 'yes' for I want to keep them, type 'no' to change them or type 'view' to see the default settings ). \n" << endl;
 		cout << ": ";
-		cin >> settingsForMoving;
+		cin >> settingsForMoving;*/
 
 		setup();
 	}
--- Sort-Bot/ItemMoving.h
+++ Sort-Bot/ItemMoving.h
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <unordered_set>
 #include "ItemsProcessing.h"
+#include "MovPrefixGlobalVector.h"
 
 
 namespace ItemMoving{
@@ -14,16 +15,17 @@ namespace ItemMoving{
 		cout << "Moving begining" << endl;
 
 		unordered_set<string> unset;
-		for (int i1 = 0; i1 < pointAmmunition_C.size(); i1++) {
-			for (PointAmmunition pointAM : pointAmmunition_C[i1]) {
-				if (unset.find(pointAM.nameOfAmmunition) == unset.end()) {
-					unset.insert(pointAM.nameOfAmmunition);
+		for (int i1 = 0; i1 < pointAmmunition_C.size(); i1++) {                  // loop through the first vector 
+			for (PointAmmunition pointAM : pointAmmunition_C[i1]) {              // take out the first point 
+				if (unset.find(pointAM.nameOfAmmunition) == unset.end()) {       // see if the first point exists 
+					unset.insert(pointAM.nameOfAmmunition);                      // it does not exist is added to use it only once 
 
-					for (int in1 = 0; in1 < pointAmmunition_C.size(); in1++) {
+					for (int in1 = 0; in1 < pointAmmunition_C.size(); in1++) {   // loop through the vector to find each of the points with the same name  
 						cout << in1 << endl;
 						for (PointAmmunition inPointAM : pointAmmunition_C[in1]) {
 							if (inPointAM.nameOfAmmunition == pointAM.nameOfAmmunition) {
 								cout << inPointAM.nameOfAmmunition << " <-inPointAm pointAM-> " << pointAM.nameOfAmmunition <<endl;
+								
 							}
 						}
 					}

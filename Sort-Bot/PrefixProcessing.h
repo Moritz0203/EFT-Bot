03f7d07a36ce1b0bf276aa8d5eadf5c27148c473
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-03-08 21:04:11 +0100

    CombinePrefixAndCase

--- Sort-Bot/PrefixProcessing.h
+++ Sort-Bot/PrefixProcessing.h
@@ -11,51 +11,65 @@ using namespace std;
 
 
 namespace PrefixProcessing {
+	void CombinePrefixAndCase();
+	void prefixSearch(shared_ptr<PointCaseInStash> ptrStash, shared_ptr<PointCaseInCase> ptrCase);
 
 	void CombinePrefixAndCase() {
 		CaseProcessor::caseProcess();
 
 		for (int i = 0; i < pointCaseInStash_C.size(); i++) {
 			for (PointCaseInStash pointCase : pointCaseInStash_C[i]) {
+				shared_ptr<PointCaseInStash> ptr = make_shared<PointCaseInStash>(pointCase);
+				prefixSearch(ptr, nullptr);
+			}
+		}
 
-				for (TagMovPrefixGroup TagMov : groupedMovPrefixGroup) {
+		for (int i = 0; i < pointCaseInCase.size(); i++) {
+			for (PointCaseInCase pointCase : pointCaseInCase[i]) {
+				shared_ptr<PointCaseInCase> ptr = make_shared<PointCaseInCase>(pointCase);
+				prefixSearch(nullptr, ptr);
+			}
+		}
+	}
 
-					if (TagMov.identyfierAsHEX == pointCase.identyfierAsHEX) {
+	void prefixSearch(shared_ptr<PointCaseInStash> ptrStash, shared_ptr<PointCaseInCase> ptrCase) {
+		for (TagMovPrefixGroup TagMov : groupedMovPrefixGroup) {
 
-						for (MovPrefixGroup movPrefix : TagMov.movPrefixGroup) {
+			if (TagMov.identyfierAsHEX == ptrStash->identyfierAsHEX || TagMov.identyfierAsHEX == ptrCase->identyfierAsHEX) {
 
-							for (Prefix prefix : movPrefix.prefix) {
+				for (MovPrefixGroup movPrefix : TagMov.movPrefixGroup) {
 
-								if (prefix.tagOfCase == pointCase.tagCase) {
+					for (Prefix prefix : movPrefix.prefix) {
 
-									if (prefix.ptr_PCIS == nullptr && prefix.ptr_PCIC == nullptr) {
+						if (prefix.tagOfCase == ptrStash->tagCase || prefix.tagOfCase == ptrCase->tagCase) {
 
-										prefix.ptr_PCIS = make_shared<PointCaseInStash>(pointCase);
-										goto breakout;							//only method to jump out of the loop and save time 
-									}
-								}
-							}
-						}
-					}
-					else // later deletet 
-					{
-						for (MovPrefixGroup movPrefix : TagMov.movPrefixGroup) {
+							if (prefix.ptr_PCIS == nullptr && prefix.ptr_PCIC == nullptr) {
 
-							for (Prefix prefix : movPrefix.prefix) {
-
-								if (prefix.tagOfCase == pointCase.tagCase) {
-
-									if (prefix.ptr_PCIS == nullptr && prefix.ptr_PCIC == nullptr) {
-
-										prefix.ptr_PCIS = make_shared<PointCaseInStash>(pointCase);
-										goto breakout;							//only method to jump out of the loop and save time 
-									}
-								}
+								prefix.ptr_PCIS = ptrStash != nullptr ? make_shared<PointCaseInStash>(*ptrStash) : nullptr;
+								prefix.ptr_PCIC = ptrCase != nullptr ? make_shared<PointCaseInCase>(*ptrCase) : nullptr;
+								return;
 							}
 						}
 					}
 				}
-				breakout:
+			}
+			else // later deletet 
+			{
+				for (MovPrefixGroup movPrefix : TagMov.movPrefixGroup) {
+
+					for (Prefix prefix : movPrefix.prefix) {
+
+						if (prefix.tagOfCase == ptrStash->tagCase || prefix.tagOfCase == ptrCase->tagCase) {
+
+							if (prefix.ptr_PCIS == nullptr && prefix.ptr_PCIC == nullptr) {
+
+								prefix.ptr_PCIS = ptrStash != nullptr ? make_shared<PointCaseInStash>(*ptrStash) : nullptr;
+								prefix.ptr_PCIC = ptrCase != nullptr ? make_shared<PointCaseInCase>(*ptrCase) : nullptr;
+								return;
+							}
+						}
+					}
+				}
 			}
 		}
 	}

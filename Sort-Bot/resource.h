f10c07505ae52df2c98853e265051f06ad2ffbd3
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-09-10 15:41:00 +0200

    Exe Image

--- Sort-Bot/resource.h
+++ Sort-Bot/resource.h
@@ -2,13 +2,13 @@
 // Microsoft Visual C++ generated include file.
 // Used by Sort-Bot.rc
 //
-#define IDI_ICON1                       122
+#define IDI_ICON1                       124
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        123
+#define _APS_NEXT_RESOURCE_VALUE        125
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101

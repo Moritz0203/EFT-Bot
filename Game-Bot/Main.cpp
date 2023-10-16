e06fee5156d5a3f4c50ae000b836203978a0e2fb
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-10-16 23:03:16 +0200

    HumanizedKeyboard HumanizedMouse

--- Game-Bot/Main.cpp
+++ Game-Bot/Main.cpp
@@ -39,56 +39,10 @@
 //	::SendInput(1, &Input, sizeof(INPUT));
 //}
 
-
-
-void MouseMoving(int xOffset, int yOffset, int durationMs) {
-    const int steps = 20;
-    const int sleepMs = durationMs / steps;
-
-    // Maximale Abweichung vom Bogen
-    int maxDeviation = 50;
-
-    // Erzeuge einen Zufallszahlengenerator für die Bogenbewegung
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> angleDist(-0.2, 0.2); // Kleine Winkelabweichung
-
-    // Easing-Funktion: Verwendet eine kubische Interpolation rückwärts (1 - (1-t)^3) für Geschwindigkeitsänderung
-    auto ease = [](double t) { return 1.0 - pow(1.0 - t, 3); };
-
-    // Berechne Schritte in x und y Richtung
-    double xStep = static_cast<double>(xOffset) / steps;
-    double yStep = static_cast<double>(yOffset) / steps;
-
-    // Mausbewegung mit allmählicher Krümmung und Easing
-    for (int step = 1; step <= steps; ++step) {
-        double t = static_cast<double>(step) / steps;
-
-        // Anwendung der Easing-Funktion auf t
-        t = ease(t);
-
-        // Allmähliche Krümmung der Bewegung
-        double curveFactor = sin(t * M_PI / 2);  // Von 0 bis 1 in Form eines Sinus
-
-        // Zufälliger Winkel entlang des Bogens
-        double angle = angleDist(gen);
-
-        // Anpasse der x- und y-Koordinaten für die Krümmung
-        double curvedX = xStep * t;
-        double curvedY = yStep * t * curveFactor;
-
-        //mouse_event(MOUSEEVENTF_MOVE, (DWORD)curvedX, (DWORD)curvedY, 0, 0);
-		//SetPosition(curvedX, curvedY);
-
-		Sleep(sleepMs); // Pausiere für eine kurze Zeit zwischen den Schritten
-    }
-}
-
-
-void SetPosition(std::int32_t x, std::int32_t y)
-{
-	mouse_event(MOUSEEVENTF_MOVE, (DWORD)x, (DWORD)y, 0, 0);
-}
+//void SetPosition(std::int32_t x, std::int32_t y)
+//{
+//	mouse_event(MOUSEEVENTF_MOVE, (DWORD)x, (DWORD)y, 0, 0);
+//}
 
 //void SetPosition(std::int32_t x, std::int32_t y)
 //{
@@ -98,13 +52,58 @@ void SetPosition(std::int32_t x, std::int32_t y)
 
 
 
+class HumanizedMouse
+{
+
+public:
+	
+	void MoveToExactPoint(int x, int y, uint speedIn_NS) {
+
+	}
+
+	void MoveToDirection(float xRotation, float yRotation, uint speedIn_NS) {
+
+	}
+};
 
 
+typedef enum e_direction {
+	Forward = 0,
+	Backwards = 1,
+}Direction;
 
+typedef enum e_rotation {
+	Left = 0,
+	Right = 1,
+	HalfLeft = 2,
+	HalfRight = 3,
+	TurnAround = 4,
+}Rotation;
+
+class HumanizedKeyboard
+{
+
+public:
+
+	void MoveToExactPosition() {
+
+	}
+
+	void MoveToDirection(Direction direction, Rotation rotation) {
+
+	}
+};
 
 
 int main() {
 	c_log::add_out(new c_log::c_log_consolestream);
+
+
+
+
+
+
+
 
 
 

d17ba49bc83f7de60d32d75236460192c55376a7
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-11-13 22:49:36 +0100

    HumanizedMovement

--- Game-Bot/HumanizedMovement.cpp
+++ Game-Bot/HumanizedMovement.cpp
@@ -1,1 +1,21 @@
 #include "HumanizedMovement.h"
+
+MoveState::MoveState(MoveType dir, bool killProcess, bool softKillProcess)
+	: moveType(dir) {
+	if (killProcess && softKillProcess) {
+		KillProcess = true;
+		SoftKillProcess = false;
+	}
+	else {
+		KillProcess = killProcess;
+		SoftKillProcess = softKillProcess;
+	}
+}
+
+void HumanizedMovement::StartMove(MoveType moveType) {
+
+}
+
+void HumanizedMovement::StopMove(MoveType moveType, bool KillProcess, bool SoftKillProcess) {
+
+}
--- Game-Bot/HumanizedMovement.h
+++ Game-Bot/HumanizedMovement.h
@@ -2,12 +2,28 @@
 #include "HumanizedKeyboard.h"
 #include "HumanizedMouse.h"
 
+typedef enum e_movetype {
+	MOVE_TYPE_STOP = 1,
+	MOVE_TYPE_BIG_CIRCLE = 2,
+	MOVE_TYPE_SMALL_CIRCLE = 3,
+	MOVE_TYPE_AUTO = 4,
+	MOVE_TYPE_RANDOM = 5,
+} MoveType;
 
+struct MoveState {
+	MoveType moveType;
+	bool KillProcess; // true = Kill Process, false = Continue Process
+	bool SoftKillProcess; // true = Soft Kill Process, false = Continue Process
+
+	MoveState(MoveType dir, bool killProcess, bool softKillProcess);
+};
 
 class HumanizedMovement : public HumanizedKeyboard, public HumanizedMouse {
 	
 
+	
 public:
 
-	
+	void StartMove(MoveType moveType = MOVE_TYPE_AUTO);
+	void StopMove(MoveType moveType, bool KillProcess, bool SoftKillProcess);
 };

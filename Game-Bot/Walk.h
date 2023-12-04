5f729b3f5fdd05364a938e5f626c8e50d788d786
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-12-04 16:58:26 +0100

    x

--- Game-Bot/Walk.h
+++ Game-Bot/Walk.h
@@ -2,7 +2,41 @@
 #include "HumanizedMovement.h"
 
 
-class Walk
-{
+class Walk {
+	bool CripWalkWhenNoStamina = false; // Set via User Input
+	MoveType MoveTypes = MoveType::MOVE_TYPE_NONE;
+	MovingCondition MovingConditions = MovingCondition::MOVING_CONDITION_NONE;
 
+public:
+
+	void SetCripWalkWhenNoStamina(bool value) {
+		if(value != CripWalkWhenNoStamina)
+			CripWalkWhenNoStamina = value;
+	}
+
+	void SetMoveType(MoveType value) {
+		if (value != MoveType::MOVE_TYPE_NONE) {
+			if (value != MoveTypes) {
+				MoveTypes = value;
+			}
+		}	
+	}
+
+	void SetMovingConditions(MovingCondition value) {
+		if(value != MovingCondition::MOVING_CONDITION_NONE){
+			if (value != MovingConditions) {
+				MovingConditions = value;
+			}		
+		}
+	}
+
+	bool GetCripWalkWhenNoStamina() {
+		return CripWalkWhenNoStamina;
+	}
+
+	Walk(bool CripWalk, MoveType moveType, MovingCondition movingCondition) {
+		SetCripWalkWhenNoStamina(CripWalk);
+		SetMoveType(moveType);
+		SetMovingConditions(movingCondition);
+	}
 };

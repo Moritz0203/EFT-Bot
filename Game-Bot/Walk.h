266ca04ba62c962f1b3f9b057b2f62d15d857d94
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-12-04 17:26:33 +0100

    x

--- Game-Bot/Walk.h
+++ Game-Bot/Walk.h
@@ -4,13 +4,22 @@
 
 class Walk {
 	bool CripWalkWhenNoStamina = false; // Set via User Input
+	bool MovingIsRunning = false;	
 	MoveType MoveTypes = MoveType::MOVE_TYPE_NONE;
 	MovingCondition MovingConditions = MovingCondition::MOVING_CONDITION_NONE;
+	HumanizedMovement humanizedMovement;
 
 public:
 
+	Walk(bool CripWalk, MoveType moveType, MovingCondition movingCondition) {
+		SetCripWalkWhenNoStamina(CripWalk);
+		SetMoveType(moveType);
+		SetMovingConditions(movingCondition);
+	}
+
+
 	void SetCripWalkWhenNoStamina(bool value) {
-		if(value != CripWalkWhenNoStamina)
+		if (value != CripWalkWhenNoStamina)
 			CripWalkWhenNoStamina = value;
 	}
 
@@ -19,14 +28,17 @@ public:
 			if (value != MoveTypes) {
 				MoveTypes = value;
 			}
-		}	
+		}
 	}
 
 	void SetMovingConditions(MovingCondition value) {
-		if(value != MovingCondition::MOVING_CONDITION_NONE){
+		if (value != MovingCondition::MOVING_CONDITION_NONE) {
 			if (value != MovingConditions) {
 				MovingConditions = value;
-			}		
+				
+				if (MovingIsRunning)
+					humanizedMovement.SetMovingCondition(MovingConditions);
+			}
 		}
 	}
 
@@ -34,9 +46,28 @@ public:
 		return CripWalkWhenNoStamina;
 	}
 
-	Walk(bool CripWalk, MoveType moveType, MovingCondition movingCondition) {
-		SetCripWalkWhenNoStamina(CripWalk);
-		SetMoveType(moveType);
-		SetMovingConditions(movingCondition);
+	void InstendStopMoving() {
+		if (MoveTypes != MoveType::MOVE_TYPE_NONE) {
+			humanizedMovement.StopMove(true, false);
+			MovingIsRunning = false;
+		}
+	}
+
+	void StopMoving() {
+		if (MoveTypes != MoveType::MOVE_TYPE_NONE) {
+			humanizedMovement.StopMove(false, true);
+			MovingIsRunning = false;
+		}
+	}
+
+	void StartMoving() {
+		if (MoveTypes != MoveType::MOVE_TYPE_NONE) {
+			if (MovingConditions != MovingCondition::MOVING_CONDITION_NONE) {
+				humanizedMovement.StartMove(MoveTypes);
+				humanizedMovement.SetMovingCondition(MovingConditions);
+
+				MovingIsRunning = true;
+			}
+		}
 	}
 };
